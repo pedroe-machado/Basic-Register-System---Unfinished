065ec41586ef845802e83b93c07f5c8c5259791e
paje <pedro.mdpn@gmail.com>
2023-02-15 14:08:26 -0300

    implementando ordenação

--- main.cpp
+++ main.cpp
@@ -14,12 +14,32 @@ struct registro {
 
 };
 
-void quicksort(int a[], int pos_pivo, int fim) {
+void swap(int vetor[],int pos1, int pos2){
+    int aux = vetor[pos1];
+    vetor[pos1] = vetor[pos2];
+    vetor[pos2] = aux;
+}
+
+int part(int vetor[], int p, int r) { // Lomuto
+   int pivo = vetor[r];
+   int j = p;
+   int k;
+   for (k = p; k < r; k++) {
+      if (vetor[k] <= pivo) {
+         swap(vetor,j,k);
+         j++; 
+      } 
+   }
+   swap(vetor,j,r);
+   return j; 
+}
+
+void quickSort(int vetor[], int pos_pivo, int fim) {
    int pos_novo_pivo;         
    if (pos_pivo < fim) {  
-      pos_novo_pivo = particiona(a, pos_pivo, fim);
-      quicksort(a, pos_pivo, pos_novo_pivo - 1); 
-      quicksort(a, pos_novo_pivo + 1, fim); 
+      pos_novo_pivo = part(vetor, pos_pivo, fim);
+      quickSort(vetor, pos_pivo, pos_novo_pivo - 1); 
+      quickSort(vetor, pos_novo_pivo + 1, fim); 
    }
 }
 
