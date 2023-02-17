92249f900979e9d41a97258876d483100b78cad2
paje <pedro.mdpn@gmail.com>
2023-02-17 09:31:12 -0300

    bin

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+/* PROJETO PRATICO IALG
+    Nomes: Ana Elis Naves / Beatriz Euzébio / Pedro Ernesto
+    */
+
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <stdio.h>
 
 using namespace std;
-
 struct registro {
 
     int ID;
@@ -13,6 +17,28 @@ struct registro {
     float precoVenda;
 
 };
+
+int Converte() { //converte o arquivo em binario
+    std::ifstream infile("base8.csv"); // Abrir o arquivo CSV
+    std::ofstream outfile("base08.bin", std::ios::out | std::ios::binary); // Criar o arquivo binário
+
+    std::string linha;
+    while (std::getline(infile, linha)) { // Ler cada linha do arquivo CSV
+        int inteiro;
+        std::string texto;
+        std::stringstream ss(linha);
+        std::getline(ss, texto, ','); // Extrair o valor inteiro
+        inteiro = std::stoi(texto);
+        std::getline(ss, texto); // Extrair a string
+        outfile.write(reinterpret_cast<char*>(&inteiro), sizeof(int)); // Escrever o valor inteiro no arquivo binário
+        outfile.write(texto.c_str(), texto.size()); // Escrever a string no arquivo binário
+    }
+
+    infile.close(); // Fechar o arquivo CSV
+    outfile.close(); // Fechar o arquivo binário
+    return 0;
+}
+
 
 void swap(int vetor[],int pos1, int pos2){
     int aux = vetor[pos1];
@@ -34,13 +60,22 @@ int part(int vetor[], int p, int r) { // Lomuto
    return j; 
 }
 
-void quickSort(int vetor[], int pos_pivo, int fim) {
+void quickSortID(int ID[], int pos_pivo, int fim) {
    int pos_novo_pivo;         
    if (pos_pivo < fim) {  
-      pos_novo_pivo = part(vetor, pos_pivo, fim);
-      quickSort(vetor, pos_pivo, pos_novo_pivo - 1); 
-      quickSort(vetor, pos_novo_pivo + 1, fim); 
+      pos_novo_pivo = part(ID, pos_pivo, fim);
+      quickSortID(ID, pos_pivo, pos_novo_pivo - 1); 
+      quickSortID(ID, pos_novo_pivo + 1, fim); 
    }
+}
+
+void quickSortprecoVenda (float precoVenda, float pospivo, float fim2){
+    float posnovopivo ;
+    if (pospivo < fim2) {
+        posnovopivo = part (precoVenda, pospivo, fim2);
+        quickSortprecoVenda (precoVenda, pospivo, posnovopivo -1);
+        quickSortprecoVenda (precoVenda, posnovopivo +1, fim2);
+    }
 }
 
 void ordena(int opcao){ //quicksort
@@ -92,5 +127,3 @@ void insere(registro novo){
 
 
 } //ordenado
-
-
