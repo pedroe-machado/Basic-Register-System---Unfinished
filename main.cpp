d484aba8807e69b013766320f875afa264d8768e
paje <pedro.mdpn@gmail.com>
2023-02-17 18:02:47 -0300

    menu e ordenação ok

--- main.cpp
+++ main.cpp
@@ -40,18 +40,18 @@ int Converte() { //converte o arquivo em binario
 }
 
 
-void swap(int vetor[],int pos1, int pos2){
-    int aux = vetor[pos1];
+void swap(registro vetor[],int pos1, int pos2){
+    registro aux = vetor[pos1];
     vetor[pos1] = vetor[pos2];
     vetor[pos2] = aux;
 }
 
-int part(int vetor[], int p, int r) { // Lomuto
-   int pivo = vetor[r];
+int partID(registro vetor[], int p, int r) { // Lomuto
+   int pivo = vetor[r].ID;
    int j = p;
    int k;
    for (k = p; k < r; k++) {
-      if (vetor[k] <= pivo) {
+      if (vetor[k].ID <= pivo) {
          swap(vetor,j,k);
          j++; 
       } 
@@ -60,54 +60,70 @@ int part(int vetor[], int p, int r) { // Lomuto
    return j; 
 }
 
-void quickSortID(int ID[], int pos_pivo, int fim) {
+int partPV(registro vetor[], int p, int r) { // Lomuto
+   int pivo = vetor[r].precoVenda;
+   int j = p;
+   int k;
+   for (k = p; k < r; k++) {
+      if (vetor[k].precoVenda <= pivo) {
+         swap(vetor,j,k);
+         j++; 
+      } 
+   }
+   swap(vetor,j,r);
+   return j; 
+}
+
+void quickSortID(registro vetor[], int pos_pivo, int fim) {
    int pos_novo_pivo;         
    if (pos_pivo < fim) {  
-      pos_novo_pivo = part(ID, pos_pivo, fim);
-      quickSortID(ID, pos_pivo, pos_novo_pivo - 1); 
-      quickSortID(ID, pos_novo_pivo + 1, fim); 
+      pos_novo_pivo = partID(vetor, pos_pivo, fim);
+      quickSortID(vetor, pos_pivo, pos_novo_pivo - 1); 
+      quickSortID(vetor, pos_novo_pivo + 1, fim); 
    }
 }
 
-void quickSortprecoVenda (float precoVenda, float pospivo, float fim2){
+void quickSortprecoVenda (registro vetor[], int pospivo, int fim){
     float posnovopivo ;
-    if (pospivo < fim2) {
-        posnovopivo = part (precoVenda, pospivo, fim2);
-        quickSortprecoVenda (precoVenda, pospivo, posnovopivo -1);
-        quickSortprecoVenda (precoVenda, posnovopivo +1, fim2);
+    if (pospivo < fim) {
+        posnovopivo = partPV(vetor, pospivo, fim);
+        quickSortprecoVenda (vetor, pospivo, posnovopivo-1);
+        quickSortprecoVenda (vetor, posnovopivo+1, fim);
     }
 }
 
-void ordena(int opcao){ //quicksort
-
-    switch (opcao){
-
-    case 0: //ordena por ID
-
-        break;
-    
-    case 1: //ordena por preço de venda
-
-        break;
-    
-    default:
-
-        break;
-    }
+int menu(){		// Criado um menu onde o usuário escolhe uma opcao numerica.
+    int opc;
+    cout << "- - - - - - - -  MENU  - - - - - - - - -" << endl << endl;
+    cout << " [ 1 ] -  Cadastrar " << endl;
+    cout << " [ 2 ] -  Listar " << endl;
+    cout << " [ 3 ] -  Pesquisar um item " << endl;
+    cout << " [ 5 ] -  Excluir " << endl;
+    cout << " [ 6 ] -  Sair " << endl << endl;
+    cout << "Digite sua opcao: ";
+    cin >> opc;
+    return opc;
 }
 
-int binRecursiva(int vetor[], int inicio, int fim, int buscado) {     
+int binRecursivaID(registro vetor[], int inicio, int fim, int buscado) {     
     
     if (inicio <= fim){
         int meio = (inicio+fim)/2;
-        if (buscado > vetor[meio]) return binRecursiva(vetor,meio+1,fim,buscado);
-        else if (buscado < vetor[meio]) return binRecursiva(vetor,inicio,meio-1,buscado);
+        if (buscado > vetor[meio].ID) return binRecursiva(vetor,meio+1,fim,buscado);
+        else if (buscado < vetor[meio].ID) return binRecursiva(vetor,inicio,meio-1,buscado);
         else return meio;
     } return -1;
 
 }
 
-void apaga(registro &vetor[], int pos){ //marca id negativo 
+int buscaDescritor(registro vetor[], string buscado) {     
+    
+    while()
+
+}
+
+
+void apaga(registro vetor[], int pos){ //marca id negativo 
     int aux = vetor[pos].ID;
     vetor[pos].ID = -1*aux;
 }
